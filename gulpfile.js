9edd72d0caac945216c79fd4b4c2635d88613008
awmleer <awmleer@sparker.xyz>
2018-10-15 11:18:28 +0800

    clean output dir before build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 
-gulp.task('prebuild', function() {
+gulp.task('clean-lib', function () {
+  return gulp.src('lib', { read: false })
+    .pipe(clean());
+});
+
+gulp.task('prebuild', ['clean-lib'], function() {
   return gulp.src('src/**')
     .pipe(gulp.dest('lib/'));
 });
