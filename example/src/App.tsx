833b16c6f30e60aebefe547d9023f5b22f95f0d7
awmleer <awmleer@sparker.xyz>
2018-10-17 17:27:34 +0800

    test nested bloc provider

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -12,16 +12,46 @@ export class App extends React.Component<{}, {}> {
         <CounterBloc.Provider>
           <CounterBloc.Consumer>
             {(counterBloc:CounterBloc) => (
-              <Subscribe to={[counterBloc.count$, counterBloc.countAnother$]}>
-                {(count, countAnother) => (
-                  <div>
-                    <p>{count}</p>
-                    <p>{countAnother}</p>
-                  </div>
-                )}
-              </Subscribe>
+              <React.Fragment>
+                <Subscribe to={[counterBloc.count$, counterBloc.countAnother$]}>
+                  {(count, countAnother) => (
+                    <div>
+                      <p>{count}</p>
+                      <p>{countAnother}</p>
+                    </div>
+                  )}
+                </Subscribe>
+                <Subscribe to={counterBloc.shouldShowResetButton$}>
+                  {data => {
+                    console.log(data)
+                    return data && <button onClick={counterBloc.resetCounter}>reset</button>
+                  }}
+                </Subscribe>
+              </React.Fragment>
             )}
           </CounterBloc.Consumer>
+          <CounterBloc.Provider>
+            <CounterBloc.Consumer>
+              {(counterBloc:CounterBloc) => (
+                <React.Fragment>
+                  <Subscribe to={[counterBloc.count$, counterBloc.countAnother$]}>
+                    {(count, countAnother) => (
+                      <div>
+                        <p>{count}</p>
+                        <p>{countAnother}</p>
+                      </div>
+                    )}
+                  </Subscribe>
+                  <Subscribe to={counterBloc.shouldShowResetButton$}>
+                    {data => {
+                      console.log(data)
+                      return data && <button onClick={counterBloc.resetCounter}>reset</button>
+                    }}
+                  </Subscribe>
+                </React.Fragment>
+              )}
+            </CounterBloc.Consumer>
+          </CounterBloc.Provider>
         </CounterBloc.Provider>
       </div>
     )
