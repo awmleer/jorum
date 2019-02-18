796e52c0eb6574ed05302ef9fbc6cb9495562bdc
awmleer <awmleer@sparker.xyz>
2019-02-18 17:39:26 +0800

    rename param inputs to deps in useSubscription

--- src/subscribe.tsx
+++ src/subscribe.tsx
@@ -113,13 +113,13 @@ export function useStream<T>(stream: Subscribable<T>, initialValue?: T): T {
   return state
 }
 
-export function useSubscription<T>(stream: Subscribable<T>, observer: PartialObserver<T>, inputs?: any[]): void
-export function useSubscription<T>(stream: Subscribable<T>, next: (value: T) => void, inputs?: any[]): void
-export function useSubscription<T>(stream: Subscribable<T>, subscribeArg: any, inputs: any[] = []): void {
+export function useSubscription<T>(stream: Subscribable<T>, observer: PartialObserver<T>, deps?: any[]): void
+export function useSubscription<T>(stream: Subscribable<T>, next: (value: T) => void, deps?: any[]): void
+export function useSubscription<T>(stream: Subscribable<T>, subscribeArg: any, deps: any[] = []): void {
   useEffect(() => {
     if (stream) {
       const subscription = stream.subscribe(subscribeArg)
       return subscription.unsubscribe.bind(subscription)
     }
-  },[stream, ...inputs])
+  },[stream, ...deps])
 }
